a32cff63bf4300f29fd580090cccaea0203c5af6
Aurélie Besnard <aurelie.besnard@aum.bio>
2022-02-16 20:31:23 +0100

    ajout d'un marker dans le détail

--- frontend/src/app/activity/activity-list/activity-list.component.ts
+++ frontend/src/app/activity/activity-list/activity-list.component.ts
@@ -84,9 +84,7 @@ export class ActivityListComponent implements OnInit {
   public road: any
   public postCode: any
   public catTypes: CatDesc[] = catTypes
-
   public catType: CatDesc;
- 
 
   constructor(
     private http: HttpClient,
@@ -176,7 +174,6 @@ export class ActivityListComponent implements OnInit {
 
 
   public async geodecode(lat: any, lng: any, id: number, content: string, title: string, age: any, categorie: any) {
-    console.log(categorie, "ds geodecode")
     let url = `https://nominatim.openstreetmap.org/reverse/?format=json&&lat=${lat}&lon=${lng}`;
     let resp = await fetch(url);
     let datas = await resp.json();
@@ -194,14 +191,11 @@ export class ActivityListComponent implements OnInit {
   }
 
   piSelected(id: number) {
-    // console.log()
     this.http.get < RUN.PointInteret[] > (BASE_URL + `/pi/${id}`, {
         withCredentials: true
       })
       .subscribe((response: RUN.PointInteret[]) => {
         this.pi = response
-        // this.showCategorieName(id)
-        console.log(this.categorieName, 'categorie envoyéé')
         const NavigationExtras: NavigationExtras = {
 
           state: {
--- frontend/src/app/detail-pi/detail-pi.component.ts
+++ frontend/src/app/detail-pi/detail-pi.component.ts
@@ -1,9 +1,16 @@
-import { Component, OnInit, Output,Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  Input
+} from '@angular/core';
 import * as L from 'leaflet';
 import {
   RUN
 } from 'src/app/interfaces/PI';
-import { Router } from '@angular/router';
+import {
+  Router
+} from '@angular/router';
 
 @Component({
   selector: 'app-detail-pi',
@@ -12,40 +19,43 @@ import { Router } from '@angular/router';
 })
 export class DetailPiComponent implements OnInit {
 
-  @Output() public pi : RUN.PointInteret[]= []
-
+  @Output() public pi: RUN.PointInteret[] = []
 
   public categories: RUN.Categorie[] = []
   public ages: RUN.Age[] = []
   public map: any
-  public address : string = ""
-  public road : string = ""
-  public postCode : number = 0
-  public title : string = ""
-  public content : string = ""
-  public id : number= 0
-  public lat : number = 0
-  public lng : number = 0
-
+  public address: string = ""
+  public road: string = ""
+  public postCode: number = 0
+  public title: string = ""
+  public content: string = ""
+  public id: number = 0
+  public lat: number = 0
+  public lng: number = 0
+  public markerRed = L.icon({
+    iconUrl: 'assets/images/markerRed.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [0, -41]
+  });
 
   constructor(
-    private router:Router
+    private router: Router
   ) {
     const navigation: any = this.router.getCurrentNavigation();
-    const state = navigation.extras.state as
-    {
-     
-        id: 0,
-        address : ""
-        content: ""
-        title: ""
-        lat: 0
-        lng:0
-        age:[]
-        categorie:[],
-        road : "",
-        postCode : 0
-      
+    const state = navigation.extras.state as {
+
+      id: 0,
+      address: ""
+      content: ""
+      title: ""
+      lat: 0
+      lng: 0
+      age: []
+      categorie: [],
+      road: "",
+      postCode: 0
+
     };
     this.address = state.address
     this.id = state.id
@@ -57,23 +67,26 @@ export class DetailPiComponent implements OnInit {
     this.categories = state.categorie
     this.road = state.road
     this.postCode = state.postCode
-   }
+  }
 
   ngOnInit(): void {
     const centre = {
       lat: this.lat,
       lng: this.lng
+
     };
 
     this.map = L.map('map').setView(centre, 10);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
-    const marker = L.marker([this.lat, this.lng]).addTo(this.map);
-          marker.bindPopup("L'activité se trouve ici").openPopup();
+    const marker = L.marker([this.lat, this.lng], {
+      icon: this.markerRed
+    }).addTo(this.map);
+    marker.bindPopup("L'activité se trouve ici").openPopup();
   }
 
-  navigateList(){
+  navigateList() {
     this.router.navigateByUrl("/activity")
   }
 
