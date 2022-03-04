b8d5d41f48638ece2569e23fe57abf54013397c6
Aurélie Besnard <aurelie.besnard@aum.bio>
2022-03-04 11:12:34 +0100

    affichage de tous les pic

--- frontend/src/app/pic/list-pic/list-pic.component.ts
+++ frontend/src/app/pic/list-pic/list-pic.component.ts
@@ -11,20 +11,28 @@ import { BASE_URL } from '../../global';
 export class ListPicComponent implements OnInit {
 
   public pic: RUN.PIC[]= []
+  public name : string = ""
+  public categorie : string = ""
+  public lat : number = 0
+  public lng : number = 0
   constructor(
     private http: HttpClient
   ) { }
 
   ngOnInit(): void {
    this.showAllPic()
-   console.log(this.pic, 'pic')
   }
 
   public showAllPic() {
     return this.http.get < RUN.PIC[] > (BASE_URL + '/pic/', {
       withCredentials: true})
       .subscribe((response: RUN.PIC[]) => {
-        this.pic = response})
+        console.log(response, 'reponse')
+        this.pic = response
+      }
+        )
+      
     }
+
   }
 
