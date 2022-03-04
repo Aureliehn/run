e8b1d5d929f2a81ff7739c0d5de8d1e6a175ccdb
Aurélie Besnard <aurelie.besnard@aum.bio>
2022-03-04 09:29:31 +0100

    ajout du formulaire, ajout de la carte et de la possibilité de recherche d'adresse

--- frontend/src/app/pic/form-pic/form-pic.component.ts
+++ frontend/src/app/pic/form-pic/form-pic.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import * as L from 'leaflet';
+import {
+  BASE_URL
+} from '../../global';
+import {
+  HttpClient
+} from '@angular/common/http';
+import {
+  Router
+} from '@angular/router';
+import Geocoder from 'leaflet-control-geocoder'
 
 @Component({
   selector: 'app-form-pic',
@@ -6,10 +20,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-pic.component.css']
 })
 export class FormPicComponent implements OnInit {
-
-  constructor() { }
+  public addres : string = ""
+  constructor() {}
 
   ngOnInit(): void {
+    let macarte = L.map('carte').setView([46, 2], 6)
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+      attribution: 'données OpenSreetMap France',
+      minZoom: 1,
+      maxZoom: 20
+    }).addTo(macarte)
+    const GeocoderControl = new Geocoder();
+    GeocoderControl.addTo(macarte);
+    GeocoderControl.on('markgeocode', function (e) {
+      console.log(e.geocode.name)
+    })
   }
 
 }
